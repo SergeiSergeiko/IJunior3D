bd1a9828fc44dd8f62a6695815d9e39ddb2b1aae
s.sergei <s.sergeiko.blr@gmail.com>
2024-06-01 10:54:38 +0200

    generation_of_enemies_at_the_level - Initial

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,49 +1,46 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Transform[] _spawnpoints;
-    [SerializeField] private Cube _prefab;
-    [SerializeField] private int _poolCapacity;
-    [SerializeField] private int _poolMaxSize;
-    [SerializeField] private int _delaySpawn;
+    [SerializeField] private Enemy _prefab;
+    [SerializeField] private float _delaySpawn;
 
-    private ObjectPool<Cube> _pool;
-
-    private void Awake()
-    {
-        _pool = new ObjectPool<Cube>(
-            createFunc: () => Instantiate(_prefab),
-            actionOnGet: (cube) => ActionOnGet(cube),
-            actionOnRelease: (cube) => cube.gameObject.SetActive(false),
-            actionOnDestroy: (cube) => Destroy(cube),
-            collectionCheck: true,
-            defaultCapacity: _poolCapacity,
-            maxSize: _poolMaxSize);
-    }
+    private Coroutine _spawnRepeating;
 
     private void Start()
     {
-        InvokeRepeating(nameof(Spawn), 0f, _delaySpawn);
+        StartCoroutine(ref _spawnRepeating, SpawnRepeating());
+    }
+
+    private IEnumerator SpawnRepeating()
+    {
+        WaitForSeconds wait = new(_delaySpawn);
+
+        while (true)
+        {
+            Spawn();
+
+            yield return wait;
+        }
     }
 
     private void Spawn()
     {
-        _pool.Get();
+        Vector3 direction = GetDirectionRandomly();
+        Vector3 position = GetSpawnPointRandomly();
+
+        Enemy cube = Instantiate(_prefab, position, Quaternion.identity);
+        cube.Init(direction);
     }
 
-    private void ActionOnGet(Cube cube)
+    private Vector3 GetDirectionRandomly()
     {
-        cube.transform.position = GetSpawnPointRandomly();
-        cube.gameObject.SetActive(true);
-        cube.Died += CubeDied;
-    }
+        Vector3[] directions = new[] { Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+        int randomIndex = Random.Range(0, directions.Length);
 
-    private void CubeDied(Cube cube)
-    {
-        cube.Died -= CubeDied;
-        _pool.Release(cube);
+        return directions[randomIndex];
     }
 
     private Vector3 GetSpawnPointRandomly()
@@ -51,5 +48,13 @@ public class Spawner : MonoBehaviour
         int index = Random.Range(0, _spawnpoints.Length);
 
         return _spawnpoints[index].position;
+    }
+
+    private void StartCoroutine(ref Coroutine coroutine, IEnumerator method)
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+
+        StartCoroutine(method);
     }
 }
